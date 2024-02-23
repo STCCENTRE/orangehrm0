b21166003a911bad5a15a6436e29bdf08e8f1bc7
Karthik <karthik.karthi4024@gmail.com>
2024-02-23 11:39:15 +0530

    lines added

--- Orangehrm0/src/module1/Login.java
+++ Orangehrm0/src/module1/Login.java
@@ -1,5 +1,26 @@
 package module1;
 
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
 public class Login {
 
+	@Test
+	public void user() {
+		
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\USER\\Desktop\\web drivers\\Gecko driver\\geckodriver.exe");
+		
+		
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		
+		driver.get("http://localhost/orangehrm/orangehrm-5.6/web/index.php/auth/login");
+	}
 }

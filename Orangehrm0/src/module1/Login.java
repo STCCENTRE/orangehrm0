afb7f174bcd3aff1678d36495b13d299d32058a4
Jency <jencyida@gmail.com>
2024-02-23 11:43:56 +0530

    Lines Added

--- Orangehrm0/src/module1/Login.java
+++ Orangehrm0/src/module1/Login.java
@@ -1,5 +1,20 @@
 package module1;
+import java.awt.AWTException;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 public class Login {
-
+	
+	public static void main(String[] args) throws AWTException, InterruptedException
+	{
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\LENOVO\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://localhost/orangehrm/orangehrm-5.6/orangehrm-5.6/web/index.php/auth/login");
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("Jency@12345");
+		driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+}
 }
